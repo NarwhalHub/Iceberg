1ebbaed93437740389943446dcac62427f71ae12
Puleeno Nguyen <puleeno@gmail.com>
2019-10-17 22:42:52 +0700

    Added based project structure

---
+++ wp-manads.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Manads
+ * Plugin URI: https://puleeno.com
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Description: The flexible ads manager for WordPress
+ * Version: 0.1.0
+ * Tag: ads, manager, performance
+ */
+
+define('WP_MANADS_PLUGIN_FILE', __FILE__);
+
