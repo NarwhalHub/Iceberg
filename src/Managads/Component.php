9f8c3d4ade07801c2a7b75656ebe258361f0c475
Puleeno Nguyen <puleeno@gmail.com>
2020-02-11 11:52:58 +0700

    Fix warning when option managads_active_components don't have value

--- src/Managads/Component.php
+++ src/Managads/Component.php
@@ -6,6 +6,10 @@ class Component
     public function loadComponents()
     {
         $activatedComponents = get_option('managads_active_components');
+        if (empty($activatedComponents)) {
+            return;
+        }
+
         foreach ($activatedComponents as $activatedComponent) {
             $component = sprintf('%s/%s', ABSPATH, $activatedComponent);
             if (file_exists($component)) {
